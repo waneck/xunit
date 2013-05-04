42ec6a29b7d83ec967b729a6ea3e5e22d17c4ac9
Caue Waneck <waneck@gmail.com>
2013-05-04 00:56:47 -0300

    issue 1750 ok

--- src/tests/issues/Issue1750.hx
+++ src/tests/issues/Issue1750.hx
@@ -2,28 +2,33 @@ package tests.issues;
 import utest.Assert;
 
 
-@:skip
 class Issue1750 extends Test
 {
-#if false
 	public function testIssue()
 	{
 		var iThing:IThing = new Thing();
 		Assert.isTrue(Type.getClass(iThing) == Thing);
-		// Assert.equals(Type.getClass(iThing), Thing);
+		Assert.equals(Type.getClass(iThing), Thing);
+		Assert.isTrue(eq(Type.getClass(iThing), Thing));
 
 		var actualThing:Thing = cast iThing;
 		Assert.isTrue(Std.is(iThing, Thing));
 	}
-#end
+
+	static function eq<T>(a:T, b:T):Bool
+	{
+		return a == b;
+	}
 }
 
-private interface IThing {
-        function toString():String;
+private interface IThing
+{
+	function toString():String;
 }
 
-private class Thing implements IThing {
-        public function new() { }
+private class Thing implements IThing
+{
+	public function new() { }
 
-        public function toString() return "Thing";
+	public function toString() return "Thing";
 }
